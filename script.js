5f9f5a4f91ef8e621ea2da8a1d5b44854c12475e
Kevin Chaney <kevin_chaney@icloud.com>
2022-06-21 10:39:30 -0700

    Finalized working code to store data from fetch into global array.

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 //fetch data for hearthstone cards 
 const api_url = 'https://us.api.blizzard.com/hearthstone/cards?locale=en_US&type=minion&access_token=USpy8HMUdraH3Xlwm609CSXp4RJOksMRd5'
 
-const ex = [];
+//async methods used here to get everything to load and work properly compared to a regular fetch. 
+//Global array int and fetch data pushed into ex 
+
+const cards = []
 async function getCards() {
     const response = await fetch(api_url);
     const data = await response.json();
-    cardData = data.cards
-    ex.push(cardData)
-    return ex
+    const cardData = data.cards
+    cardData.forEach(index => {
+        cards.push(index)
+    })
+    return cardData
 }
-getCards()
+getCards();
+
+
 
 //create buttons to compare different values of cards
 //assume we are only looking at base values without applying card effects
@@ -35,9 +42,15 @@ let userScore = 0;
 let cpuScore = 0;
 
 //Randomly select cards 
-function shuffleCards() {
-    let deckLength = cards.length;
-    let userCardCount = 0;
-    let cpuCardCount = 0;
-}
+// function shuffleCards() {
+//     let deckLength = cards.length;
+//     let userCardCount = 0;
+//     let cpuCardCount = 0;
 
+//     while(--deckLength > 0) {
+//         let cardIndex = Math.floor(Math.random() * deckLength(deckLength +1))
+//         let randomizedCard = cards.splice(cardIndex, 1);
+//         console.log(randomizedCard)
+//     }
+// }
+// shuffleCards()
