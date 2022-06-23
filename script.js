664292c6cf88a11f34a955c4792e5ab8d33ce52a
Kevin Chaney <kevin_chaney@icloud.com>
2022-06-22 20:11:35 -0700

    Changed comments made in getCards function. Added function of the main objective of the game ccomparing stats of each card. Added function to change the score of the game between the user and the cpu.

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-
-//Global array int and fetch data pushed into cards 
-
-
 async function getCards() {
     try {
-        const response = await fetch('https://us.api.blizzard.com/hearthstone/cards?locale=en_US&set=standard&collectible=1&type=minion&access_token=US7iDM0Dc1yDugE16xrGV9pMYQGKJ0rFvI');
+        const response = await fetch('https://us.api.blizzard.com/hearthstone/cards?locale=en_US&set=standard&collectible=1&rarity=legendary&type=minion&access_token=US7iDM0Dc1yDugE16xrGV9pMYQGKJ0rFvI');
         if (!response.ok){
             throw new Error(`Failed to fetcb posts: ${response.status}`)
         }
@@ -29,16 +25,21 @@ let cpuScoreDisplay = document.getElementById("cpuScore");
 let userCardPic = document.getElementById("user_Card");
 let cpuCardPic = document.getElementById("cpu_Card");
 
-//cards to be displayed
-let mainCard = []
-let cpuCard = []
+//cards to be stored in each "deck"
+let userCards = []
+let cpuCards = []
 
 //User score and cpu score
 let userScore = 0;
 let cpuScore = 0;
 
+//current cards to display
+let userCurrent = []
+let cpuCurrent = []
+
 // Randomly select cards 
 function shuffleCards() {
+    //callback getCards function, keeping mind it is an async function
     getCards().then (data => {
         let cardData = data.cards
         let deckLength = cardData.length;
@@ -49,8 +50,75 @@ function shuffleCards() {
             let cardIndex = Math.floor(Math.random() * (deckLength +1))
             let randomizedCard = cardData.splice(cardIndex, 1);
             if(userCardCount > cpuCardCount) {
-                cpuCard.push(randomizedCard[0])
+                cpuCards.push(randomizedCard[0])
+                cpuCardCount +=1;
+            }else if (userCardCount == cpuCardCount) {
+                userCards.push(randomizedCard[0]);
+                userCardCount +=1;
             }
         }
     })
+}
+
+function currentCard() {
+    //select random card from each deck
+    let user = Math.floor((Math.random() * userCards.length));
+    let cpu = Math.floor((Math.random() * userCards.length));
+
+    userCurrent.push(userCards.splice(user, 1)[0]);
+    cpuCurrent.push(cpuCards.splice(cpu, 1)[0]);
+
+    // console.log(userCurrent[0]);
+    // console.log(cpuCurrent[0]);
+
+    userCardPic.src = `${userCurrent[0].cropImage}`
+    cpuCardPic.src = `${cpuCurrent[0].cropImage}`
+}
+
+function compareStat(stat) {
+    // console.log(`the user ${stat} is: ${userCurrent[0][stat]}`)
+    // console.log(`the cpu ${stat} is: ${cpuCurrent[0][stat]}`)
+
+    let userStat = userCurrent[0][stat];
+    let cpuStat = cpuCurrent[0][stat];
+
+    //logic for choosing winner
+    if (stat == "manaCost" || stat == "attack" || stat == "health") {
+        if (userStat > cpuStat) {
+            roundResult("user win")
+        } else if (userStat < cpuStat) {
+            roundResult("cpu win");
+        }else {
+            roundResult("Draw")
+        }
+    }
+
+}
+
+function roundResult(result) {
+    if(result == "user win") {
+        userScore +=1;
+        userScoreDisplay.textContent = `Your score: ${userScore}`;
+        userCards.push(cpuCurrent.splice(0,1)[0]);
+        userCards.push(userCurrent.splice(0,1)[0]);
+        messageDisplay.textContent = "You win this round!";
+        setTimeout ( () => {
+            messageDisplay.style.display = "none"
+        }, 2000);
+    } else if (result == "cpu win") {
+        cpuScore +=1;
+        cpuScoreDisplay.textContent = `CPU score: ${cpuScore}`;
+        cpuCards.push(userCurrent.splice(0,1)[0]);
+        cpuCards.push(cpuCurrent.splice(0,1)[0]);
+        messageDisplay.textContent = "You lose this round";
+        setTimeout ( () => {
+            messageDisplay.style.display = "none"
+        }, 2000);
+    } else {
+        messageDisplay.textContent("This round is a draw");
+        userCards.push(userCurrent.splice(0,1)[0]);
+        cpuCards.push(cpuCurrent.splice(0,1)[0]);
+        playGame();
+    }
+    
 }
