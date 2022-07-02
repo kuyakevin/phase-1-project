7e8c68dbdebef54ec17d7406d577c33d4a8e762b
Kevin Chaney <kevin_chaney@icloud.com>
2022-07-02 10:51:27 -0700

    Changed the amount of available cards to 100

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getCards() {
     try {
-        let response = await fetch('https://us.api.blizzard.com/hearthstone/cards?locale=en_US&set=standard&collectible=1&rarity=legendary&type=minion&access_token=USJRpogiSMFGkngitvr83loIeTUAl0vybZ');
+        let response = await fetch('https://us.api.blizzard.com/hearthstone/cards?locale=en_US&set=standard&collectible=1&rarity=legendary&type=minion&pageSize=100&access_token=USJRpogiSMFGkngitvr83loIeTUAl0vybZ');
         if (!response.ok){
             throw new Error(`Failed to fetch posts: ${response.status}`)
         }
